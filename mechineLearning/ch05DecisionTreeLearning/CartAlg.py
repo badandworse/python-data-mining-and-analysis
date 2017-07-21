776c8fe5a8fc3a65f8987dbd3b2d2e7ccae9649e
bad <zilaixv@gmail.com>
2017-07-21 22:16:29 +0800

    reviewe chapter01

--- mechineLearning/ch05DecisionTreeLearning/CartAlg.py
+++ mechineLearning/ch05DecisionTreeLearning/CartAlg.py
@@ -52,35 +52,76 @@ filename="data.csv"
 
 dataF=readData(filepath,filename)
 
+#%%
 # 对dataframe指定列根据条件筛选赋值
 #dataF
 #dataF['age']=(dataF['age']==2).astype(int)
 
+print(1)
 charactersNum=len(dataF.columns)-1
 charactersIndex=dataF.columns[:charactersNum]
 charactersIndex
-ll=[4,2,3]
-ll.sort()
-ll
+
 #%%
 n=0
 
 #%%
-setIndex1=dataF.columns[len(dataF.columns)-1]
-minIndex=''
-minValue=10000
-setIndex1
-while True:
+# 返回下个划分元素
+def get_split(dataF,setIndex1,charactersIndex,minIndex='',minValue=1000):    
+    valueL=[]
     for m in charactersIndex:
         valuesList=getGini(dataF[[m,setIndex1]],m,setIndex1)
+        mm=valuesList.index(min(valuesList))
         valuesList.sort()
         if valuesList[0]<minValue:
             minValue=valuesList[0]
             minIndex=m
-    break
+            valueL=valuesList
+    if len(valueL)==1:
+        b_index=dataF.groupby([minIndex]).size().index
+        print(b_index)
+        left_dt=dataF[dataF[minIndex]==b_index[1]]
+        right_dt=dataF[dataF[minIndex]==b_index[0]]
+        return {'index':minIndex,'group':(left_dt,right_dt)}
+    else:
+        #即所有数据分类是多结果离散
+        b_index=dataF.groupby([minIndex]).size().index
+        dataF[minIndex]=(dataF[minIndex]==b_index[mm]).astype(int)
+        newIndex=str(b_index[mm])+'('+minIndex+')' 
+        dataF=dataF.rename(columns={minIndex:newIndex})
+        left_dt=dataF[dataF[newIndex]==1]
+        right_dt=dataF[dataF[newIndex]==0]
+        return {'index':newIndex,'group':(left_dt,right_dt)}
+      
+                     
 
-minIndex
-minValue
+
+#node=get_split(dataF[['age',dataF.columns[4]]],dataF.columns[4],[charactersIndex[0]])
+#node=get_split(dataF,dataF.columns[4],charactersIndex)
+#node
 
 
+#%%
+def split(node,max_depth,min_size,depth):
+    
+
+
+#%%
 #todo : 树的生成未完成，有点没有头绪，决定补下算法
+def build_tree(data,max_depth,min_size):
+    allIndex=data.columns
+    charactersIndex=allIndex[:len(allIndex)-1]
+    typeIndex=allIndex[len(allIndex)-1]
+    root=get_split(data,typeIndex,charactersIndex)
+    split(root,max_depth,min_size,1)
+
+
+#%%
+# data,max_depth,min_size
+tree=build_tree(dataF,3,1)
+
+#%%
+ll=[3,45,2]
+min(ll)
+ll.index(2)
+sortedll=ll.sort()
