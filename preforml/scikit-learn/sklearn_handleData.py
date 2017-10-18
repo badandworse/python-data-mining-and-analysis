3e07b6ce9d9b8b9aa643d951b669c284a31152c4
bad <zilaixv@gmail.com>
2017-10-18 21:39:47 +0800

    学习了如何使用l1正则来减少特征

--- preforml/scikit-learn/sklearn_handleData.py
+++ preforml/scikit-learn/sklearn_handleData.py
@@ -56,5 +56,64 @@ X_test_std=stdsc.transform(X_test)
 
 
 '''
-选择有意义的特征
+选择有意义的特征:
+测试效果很差，说明过拟合，可以通过降低数据的纬度来降低过拟合
+
 '''
+
+'''
+L1正则:
+权重的绝对值和，
+使得最优解很有可能落在坐标轴上,可以减少特征数
+L2正则:
+权重的平方和
+'''
+#将penalty取值设置为L1正则即可
+from sklearn.linear_model import LogisticRegression
+
+LogisticRegression(penalty='l1')
+#%%
+lr=LogisticRegression(penalty='l1',C=0.1)
+lr.fit(X_train_std,y_train)
+
+print('Training accuracy:',lr.score(X_train_std,y_train))
+
+print('Test accuracy:',lr.score(X_test_std,y_test))
+
+#只有3个值，说明lr用的one-vs-reset
+#有3个模型
+lr.intercept_
+
+lr.coef_
+
+'''
+通过画出正则以后，可以发现，C<0.1时，即正则项系数很大是，正则威力是巨大的
+使得所有特征权重都为0
+'''
+
+#%%
+import matplotlib.pyplot as plt
+sns.set()
+fig=plt.figure()
+ax=plt.subplot(111)
+colors=['blue','green','red','cyan','magenta','yellow','black','pink','lightgreen','lightblue']
+weights,params=[],[]
+for c in [-4, -3, -2, -1,  0,  1,  2,  3,  4,  5]:
+    lr=LogisticRegression(penalty='l1',C=10**c,random_state=0)
+    lr.fit(X_train_std,y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
+
+
+weights=np.array(weights)
+for column,color in zip(range(weights.shape[1]),colors):
+    plt.plot(params,weights[:,columns],label=data_wine.columns[column+1],color=color)
+
+plt.axhline(0,color='black',linestyle='--',linewidth=3)
+plt.xlim([10**(-5),10**5])
+plt.ylabel('weight cofficient')
+plt.xlabel('C')
+plt.xscale('log')
+plt.legend(loc='upper left')
+ax.legend(loc='upper center',bbox_to_anchor=(1.38,1.03),ncol=1,fancybox=True)
+plt.show() 
